a21ceebc1d48c7946bf5e58f9b9c1f0cb5f50612
anujrathore2733 <anujrathore2901@gmail.com>
2025-10-10 00:05:47 +0530

    Updates

---
+++ next/static/vsfOyCaOi3sx9bCFtog7E/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/51a110f4fd439f91.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/femure//_next/:path+",
+        "destination": "/femure/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
